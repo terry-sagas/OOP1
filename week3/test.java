3161d647d7fcd07ecb9ab759aa1f6d7684694547
terry-sagas <terrysagas2003@gmail.com>
2024-01-25 14:20:40 -0500

    added test to main branch for testing

---
+++ week3/test.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class test {
+        public static void main(String args[]){
+        
+        int age = 0;                // Setting the age variable to 0
+        String name = "";           // Setting the name variable to blank
+
+        // Setting the scanner varable to gain a input
+        Scanner scanner = new Scanner(System.in);
+
+        // Asking user for name
+        System.out.println("Enter your name: ");
+        name = scanner.nextLine();
+        System.out.println("whats your age");
+        age = scanner.nextInt();
+        System.out.println("age: " + age + "\nName: " + name);
+
+        
+        // Closing the scanner
+        scanner.close();
+        
+    }
+}
+
