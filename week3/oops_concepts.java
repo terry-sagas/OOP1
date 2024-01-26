4b486519d20f0635b84bfd6c6e4acd5a3b8aabe6
terry-sagas <terrysagas2003@gmail.com>
2024-01-25 19:05:28 -0500

    worked with classes

--- week3/oops_concepts.java
+++ week3/oops_concepts.java
@@ -1,5 +1,16 @@
 public class oops_concepts {
     public static void main(String args[]){
-        
+        // Creating the instance of student
+        // Access the methods
+        Studnet studnet = new Studnet("terry", 20);
+        System.out.println("Name: " + studnet.getName());
+        System.out.println("Age: " + studnet.getAge());
+
+        // Modify the students info
+        Studnet studnet2 = new Studnet("Theodore", 210);
+        System.out.println("Student 2 Name: " + studnet2.getName());
+        System.out.println("Student 2 Age: " + studnet2.getAge());
+
+
     }
 }
