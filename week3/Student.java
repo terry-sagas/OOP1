5c12aaa95f142ceffede2f69f8eb3c17b16ad5d4
terry-sagas <terrysagas2003@gmail.com>
2024-01-26 03:01:43 -0500

    finished lab one without part 5

---
+++ week3/Student.java
@@ -0,0 +1,45 @@
+public class Student {
+    private String fName;
+    private String lName;
+    private int age;
+
+    public Student(String fName, String lName, int age) {
+
+
+        this.fName = fName;
+        this.lName = lName;
+        this.age = age;
+    }
+
+    public String getFName() {
+        return fName;
+    }
+
+    public void setFName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLName() {
+        return lName;
+    }
+
+    public void setLName(String lName) {
+        this.lName = lName;
+        if (lName == null || lName.isEmpty()) {
+            System.out.println("Sorry, last name is needed.");
+            return;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) {
+            System.out.println("Invalid age");
+            return;
+        }
+        this.age = age;
+    }
+}
--- week3/oops_concepts.java
+++ week3/oops_concepts.java
@@ -1,16 +1,16 @@
 public class oops_concepts {
     public static void main(String args[]){
-        // Creating the instance of student
-        // Access the methods
-        Studnet studnet = new Studnet("terry", 20);
-        System.out.println("Name: " + studnet.getName());
-        System.out.println("Age: " + studnet.getAge());
+        Student student = new Student("Terry", "Sagas", 20);
+        student.setAge(20);
+        System.out.println("First Name: " + student.getFName());
+        System.out.println("Last Name: " + student.getLName());
+        System.out.println("Age: " + student.getAge());
 
         // Modify the students info
-        Studnet studnet2 = new Studnet("Theodore", 210);
-        System.out.println("Student 2 Name: " + studnet2.getName());
-        System.out.println("Student 2 Age: " + studnet2.getAge());
-
-
+        Student student2 = new Student("Theodore", "Sagas", 210);
+        student2.setAge(210);
+        System.out.println("Student 2 Name: " + student2.getFName());
+        System.out.println("Last Name: " + student2.getLName());
+        System.out.println("Student 2 Age: " + student2.getAge());
     }
 }
