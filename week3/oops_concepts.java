bc55cf3e12f861c64c2e7caee8cb572e3f158c6b
terry-sagas <terrysagas2003@gmail.com>
2024-01-26 15:56:47 -0500

    added part 5 comment

--- week3/oops_concepts.java
+++ week3/oops_concepts.java
@@ -1,6 +1,14 @@
 // Terry Sagas
 // 1/26/2025
 // oop_concepts.java
+/*
+* Part five is in other file and in other branch!
+*
+ * For part 5 i inserted the function in my code, in this case i could only set the name unless i changed the age in the 
+ * oops_concept.java file because the parameters are different and i cant use it when i call it instead i use student.setAge(20); 
+ * or else the age will appear as zero. this can be useful when i don't&nbsp;need the age for everything.&nbsp;i think the oop 
+ * concept is Polymorphism
+ */
 
 
 public class oops_concepts {
