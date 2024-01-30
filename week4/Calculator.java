2c3ad4029e0d0a2a1933002235491f9be210100c
terry-sagas <terrysagas2003@gmail.com>
2024-01-29 23:49:31 -0500

    calculator code works just needs setters, getters and comments.

--- week4/Calculator.java
+++ week4/Calculator.java
@@ -1,5 +1,65 @@
 package week4;
 
+import java.util.Scanner;
+
 public class Calculator {
-    
+        double input_one;
+        double input_two;
+        double sum;
+        double difference;
+        double product;
+        double quotient;
+        double remainder;
+
+
+        public Calculator() {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("The first decimal number: ");
+            this.input_one = scanner.nextDouble();
+
+            System.out.print("The second decimal number: ");
+            this.input_two = scanner.nextDouble();
+            scanner.close();
+        }
+
+        public double add(){
+            sum = input_one + input_two;
+            return sum;
+        }
+
+        public double subtract(){
+            difference = input_one - input_two;
+            return difference;
+        }
+
+        public double multiply(){
+            product = input_one * input_two;
+            return product;
+        }
+
+        public double divide(){
+            quotient = input_one / input_two;
+            return quotient;
+        }
+
+        public double modulus(){
+            remainder = input_one % input_two;
+            return remainder;
+        }
+
+        public void calculations()
+        {
+            System.out.println("Sum: " + add());
+            System.out.println("Difference: " + subtract());
+            System.out.println("Product: " + multiply());
+            System.out.println("Quotient: " + divide());
+            System.out.println("Remainder: " + modulus());
+        }
+
+        public static void main(String[] args) {
+            Calculator newCalculator = new Calculator();
+            newCalculator.calculations();
+        }
+
 }
